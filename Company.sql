485750097428b126ce911a5cc6b3af596fc86a03
Johan0220 <109837086+Johan0220@users.noreply.github.com>
2022-10-08 00:26:53 +0700

    Add files via upload

--- Company.sql
+++ Company.sql
@@ -6,7 +6,7 @@ go
 create table LoaiSP(
 	IDLSP int primary key identity,
 	MaLoaiSP varchar(10) not null,
-	TenLoaiSP varchar(255) not null,
+	TenLoaiSP nvarchar(255) not null,
 )
 
 
@@ -18,14 +18,26 @@ create table SanPham(
 )
 create table NhanVien(
 	MaNV int primary key,
-	TenNV varchar(20) not null,
+	TenNV nvarchar(20) not null,
 	IDSP int not null, constraint fk_SP foreign key(IDSP) references SanPham(IDSP) on delete cascade
 )
-insert into LoaiSP values('Z37','Máy tính sách tay Z37');
+alter table SanPham
+drop constraint fk_LoaiSP
+drop table SanPham
+insert into LoaiSP values('Z37E','Máy tính sách tay Z37');
 insert into SanPham values('Z37 111111','12/12/09',1);
 insert into NhanVien values('987688','Nguyễn Văn An',1);
-select * from LoaiSP
-select *
-
+select * from LoaiSP;
+select * from SanPham;
+select * from NhanVien;
+select * from LoaiSP order by TenLoaiSP;
+select * from Nhanvien order by TenNV;
+select * from LoaiSP where MaLoaiSP = 'Z37E';
+select TenLoaiSP from LoaiSP  where IDLSP in
+	(select IDLSP from SanPham where IDSP in
+		(select IDSP from NhanVien where TenNV = 'Nguyễn Văn An')) order by TenLoaiSP desc;
+select sum(IDSP) from SanPham where IDLSP in 
+	(select IDLSP from LoaiSP where TenLoaiSP = 'true')
+ 
 
 
