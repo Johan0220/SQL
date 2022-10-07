c8c2be68b5f5f278cc8e10f9fe9052e522c2fd7e
Johan0220 <109837086+Johan0220@users.noreply.github.com>
2022-10-07 23:22:50 +0700

    Add files via upload

---
+++ Company.sql
@@ -0,0 +1,31 @@
+﻿
+create database Company
+go
+use Company
+go
+create table LoaiSP(
+	IDLSP int primary key identity,
+	MaLoaiSP varchar(10) not null,
+	TenLoaiSP varchar(255) not null,
+)
+
+
+create table SanPham(
+	IDSP int primary key identity, 
+	MaSoSP varchar(20),
+	NgaySX datetime not null,
+	IDLSP int not null, constraint fk_LoaiSP foreign key(IDLSP) references LoaiSP(IDLSP) on delete cascade
+)
+create table NhanVien(
+	MaNV int primary key,
+	TenNV varchar(20) not null,
+	IDSP int not null, constraint fk_SP foreign key(IDSP) references SanPham(IDSP) on delete cascade
+)
+insert into LoaiSP values('Z37','Máy tính sách tay Z37');
+insert into SanPham values('Z37 111111','12/12/09',1);
+insert into NhanVien values('987688','Nguyễn Văn An',1);
+select * from LoaiSP
+select *
+
+
+
